2083513d92ebaa2051e81a00d38a4f9b0f29bc6c
tegarrayosdelsol-cmd <tegarrayosdelsol@gmail.com>
2025-09-21 05:36:19 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const akunList = [
   { id: 1, nama: "Akun Legend", harga: "Rp500.000", deskripsi: "Level 25, banyak pemain bintang, GP & koin melimpah.", gambar: "assets/akun1.jpg" },
   { id: 2, nama: "Akun Starter Pack", harga: "Rp 250.000", deskripsi: "Level 10, cocok untuk pemula.", gambar: "assets/akun2.jpg" },
-  { id: 3, nama: "Akun Epic Legend", harga: "Rp 3.000.000", deskripsi: "Full squad epic, cocok buat turnamen.", gambar: "assets/akun3.jpg" },
+  { id: 3, nama: "akund", harga: "Rp 3.000.000", deskripsi: "Full squad epic, cocok buat turnamen.", gambar: "assets/akun3.jpg" },
   { id: 4, nama: "Akun Garcia 🔥", harga: "Rp 750.000", deskripsi: "Level 20, punya beberapa pemain epic + koin 5.000. Cocok buat push rank.", gambar: "assets/akun1.jpg" }
 ];
